9eeaec414ebc64de7a7e4dae56ed24e6b8c8013e
vmlankub <45290401+vmlankub@users.noreply.github.com>
2020-03-06 20:41:11 +0800

    Original path: D:/orz/www.chinamobileltd.com/en/ir/analyst.php

---
+++ DJZF4vRT7GHwEAr0/ZW4$/aXI$/YW5hbHlzdC5waHA$.php
@@ -0,0 +1,372 @@
+<!doctype html>
+<html>
+<head>
+<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+<link rel="icon" href="/favicon.ico" type="image/x-icon">
+<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+<link rel="icon" type="image/png"  href="/apple-touch-icon.png">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+
+<title>China Mobile Limited - Investor Relations &gt; Analyst Coverage</title>
+<link href="../css/fonts.css" rel="stylesheet" type="text/css">
+<link href="../css/reset.css" rel="stylesheet" type="text/css">
+<link href="../css/style.css" rel="stylesheet" type="text/css">
+<link href="../css/tablet.css" rel="stylesheet" type="text/css">
+<link href="../css/desktop.css" rel="stylesheet" type="text/css">
+<link href="../../js/slick/slick.css" rel="stylesheet" type="text/css">
+<link href="../css/style1.css" rel="stylesheet" type="text/css">
+<link href="../css/style2.css" rel="stylesheet" type="text/css">
+<link href="../css/magnific-popup.css" rel="stylesheet" type="text/css">
+<link href="../css/responsive-tabs.css" rel="stylesheet" type="text/css">
+<link href="../../js/fancybox/jquery.fancybox.css?v=2.1.5" rel="stylesheet" type="text/css">
+<link href="../css/print.css" rel="stylesheet" type="text/css"></head>
+
+<body class="contentPage">
+
+<div class="wrapper">
+   <div class="headerWrapper">
+     <div id="search-popup" class="mfp-hide search">
+		 <h1>Search</h1>
+		 <div class="search-container">
+			<form method="get" action="../global/search.php">
+		 	<input type="text" id="search" value="" name="q" id="q"   placeholder="Search">
+			<span><input type="image" src="../images/global/searchicon.svg" width="100px" height="40px" alt="submit" style="margin-bottom:5px;"></span>
+			</form>
+			
+			
+			
+		 </div>
+	 </div>
+     <div id="tools-popup" class="mfp-hide">
+		 <h1>Tools</h1>
+		 <div class="search-container">
+		 <ul class="tools">
+			                  <li><a href="../global/rss.php"><div class="icon"><img src="../images/global/icon_rss.png" alt=""/></div>RSS Feeds</a></li>
+                 <li class="print"><a href="#"><div class="icon"><img src="../images/global/icon_print.png" alt=""/></div>Print this Page</a></li>
+                 <li class="bookmark"><a href="#" class="bookmark"><div class="icon"><img src="../images/global/icon_add.png" alt=""/></div>Add to Favourites</a></li>
+                 <li><a href="../webform/emailpage.php?page=https://www.chinamobileltd.com/en/ir/analyst.php" id="emailpage" ><div class="icon"><img src="../images/global/icon_email.png" alt=""/></div>Email this Page</a></li>
+                 <li><a href="#" class="addthis_button"><div class="icon"><img src="../images/global/icon_share.png" alt=""/></div>Share this Page</a></li>
+                 <li><a href="../download/index.php"><div class="icon"><img src="../images/global/icon_downloadtools.png" alt=""/></div>Download Centre</a></li>
+                 <div class="clear"></div>
+            </ul>
+		 </div>
+	 </div>
+     <div id="language-popup" class="mfp-hide">
+		 <h1>Language</h1>
+		 <div class="search-container">
+			 <div><a href="javascript:changeLanguage('en');" class="act">EN</a>  <a href="javascript:changeLanguage('tc');">繁體</a>  <a href="javascript:changeLanguage('sc');">简体</a> </div>
+		 </div>
+	 </div>
+      <div class="row header">
+		  <div class="fleft">
+			  <div class="logo"><a href="../global/home.php"><img src="../images/global/logo.svg" alt=""/></a></div>
+		  </div>
+		  <div class="fright">
+		  	  
+			  <div class="topIcon">
+			  	<ul>
+				   <li><a href="#search-popup" class="maintools-popup"><img src="../images/global/icon_search.svg" alt=""/></a></li>
+				   <li><a href="#tools-popup" class="maintools-popup"><img src="../images/global/icon_tools.svg" alt=""/></a></li>
+				   <li><a href="#language-popup" class="maintools-popup"><img src="../images/global/icon_language.svg" alt=""/></a></li>
+				   <li class="hamburger"><a href="javascript:void(0);" onclick="openNav()"><img src="../images/global/icon_hamburger.svg" alt=""/></a></li>
+				</ul>
+			  </div>
+			  <div class="topnavWrapper navControl">
+	<ul class="topnav">
+        <li class=""><a href="javascript:void(0);">About China Mobile</a>
+        	<div class="subnav">
+          	  <div class="row">
+          	  	<div class="subAbout">
+				  <ul>
+					 <li><a href="../about/overview.php">Overview</a></li>
+					 <li><a href="../about/chairman.php">Chairman's Statement</a></li>
+					 <li><a href="../about/directors.php">Board of Directors and Senior Management</a></li>
+					 <li><a href="../about/milestones.php">Milestones</a></li>
+				  </ul>
+				  <ul>
+					 <li><a href="../about/awards.php">Recognition & Awards</a></li>
+					 <li><a href="../about/rating.php">Credit Ratings</a></li>
+					 <li><a href="../about/cg.php">Corporate Governance</a></li>
+					 <li><a href="../about/sd.php">Sustainable Development</a></li>
+				  </ul>
+				</div>
+				</div>
+            </div>
+         </li>
+         <li class=""><a href="javascript:void(0);">Investor Relations</a>
+         	<div class="subnav">
+          	  <div class="row">
+          	  	<div class="subIr">
+				  <ul>
+					 <li><a href="../ir/operation_m.php">Monthly Customer Data</a></li>
+					 <li><a href="../ir/operation_q.php">Key Operation Data</a></li>
+					 <li><a href="../ir/financial.php">Financial Review</a></li>
+					 <li><a href="../ir/stockquote.php">Shareholders Service</a></li>
+				  </ul>
+				  <ul>
+					 <li><a href="../ir/reports.php">Financial Reports</a></li>
+					 <li><a href="../ir/webcasts.php">Presentations / Webcasts</a></li>
+				  </ul>
+					<ul>
+					 <li><a href="../ir/announcements.php">Announcements</a></li>
+					 <li><a href="../ir/sec.php">US SEC Filings</a></li>
+					 <li><a href="../ir/circulars.php">Circulars & Other Documents</a></li>
+					 <li><a href="../ir/faq.php">FAQs</a></li>
+					</ul>
+					<ul>				 
+					 <li><a href="../ir/corpactions.php">Corporate Actions</a></li>
+					 <li><a href="../ir/ircalendar.php">IR Calendar</a></li>
+					 <li><a href="../ir/analyst.php">Analyst Coverage</a></li>
+		             <li><a href="../ir/ircontact.php">IR Contact</a></li>
+					</ul>
+				</div>
+				</div>
+            </div>
+         </li>
+         <li class=""><a href="javascript:void(0);">Media Centre</a>
+        		<div class="subnav">
+          	  <div class="row">
+         		<div class="subMedia">
+				  <ul>
+					 <li><a href="../media/press.php">Press Releases</a></li>
+					 <li><a href="../media/news.php">News Highlights</a></li>
+					 <li><a href="../media/gallery.php">Photo Gallery</a></li>
+					 <li><a href="../media/video.php">Video Gallery</a></li>
+				  </ul>
+				  <ul>
+					 <li><a href="../media/corpimage.php">Corporate Images</a></li>
+					 <li><a href="../media/contact.php">Media Contact</a></li>
+				  </ul>
+				</div>
+					</div>
+			 </div>
+         </li>
+         <li class=""><a href="javascript:void(0);">Our Business</a>
+         	<div class="subnav">
+          	  <div class="row">
+          	  	<div class="subBusiness">
+				  <ul>
+					 <li><a href="../business/business.php">Business Review</a></li>
+					 <li><a href="../business/service.php">Products & Services</a></li>
+					 <li><a href="../business/networks.php">Networks & Technologies</a></li>
+				  </ul>
+				</div>
+				</div>
+            </div>
+         </li>     
+	</ul>
+</div>			  <div class="clear"></div>
+		  </div>
+		  <div class="clear"></div>
+      </div>
+</div>
+   
+     <div class="subBanner img2"></div>
+   <div class="fullWidth">
+   	<div class="row">
+    	<div class="titleWrapper">
+            <div class="title">Analyst Coverage</div>
+         </div>
+    </div>
+   </div>
+   <div class="contentWrapper">
+      <div class="row">
+         <!-- Content Area Start-->
+		 <!--<div class="subTitle"><a href="javascript:void(0);">SubTitle</a></div>-->
+         <div class="tableStyle01 alignleft">
+			<table width="100%" border="0" cellspacing="0" cellpadding="0">
+				<thead>
+					<tr>
+						<th width="50%">Company / Contact Person</th>
+						<th>Email Address</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>Bank of America Merrill Lynch / Danny Chu</td>
+						<td><a href="mailto:danny.wk.chu@baml.com">danny.wk.chu@baml.com</a></td>
+					</tr>
+					<tr>
+						<td>Beijing Gaohua Securities / Tina Hou</td>
+						<td><a href="mailto:tina.hou@ghsl.cn">tina.hou@ghsl.cn</a></td>
+					</tr>
+					<tr>
+						<td>BOCI / Michael Meng</td>
+						<td><a href="mailto:michael.meng@bocigroup.com">michael.meng@bocigroup.com</a></td>
+					</tr>
+					<tr>
+						<td>CICC / Kai Qian</td>
+						<td><a href="mailto:kai.qian@cicc.com.cn">kai.qian@cicc.com.cn</a></td>
+					</tr>
+					<tr>
+						<td>Citigroup / Bin Liu</td>
+						<td><a href="mailto:bin.liu@citi.com">bin.liu@citi.com</a></td>
+					</tr>
+					<tr>
+						<td>CLSA / Elinor Leung</td>
+						<td><a href="mailto:elinor.leung@clsa.com">elinor.leung@clsa.com</a></td>
+					</tr>
+					<tr>
+						<td>Credit Suisse / Colin McCallum</td>
+						<td><a href="mailto:colin.mccallum@credit-suisse.com">colin.mccallum@credit-suisse.com</a></td>
+					</tr>
+					<tr>
+						<td>Daiwa / Ramakrishna Maruvada</td>
+						<td><a href="mailto:ramakrishna.maruvada@sg.daiwacm.com">ramakrishna.maruvada@sg.daiwacm.com</a></td>
+					</tr>
+					<tr>
+						<td>DBS Vickers / Tsz Wang Tam</td>
+						<td><a href="mailto:tszwangtam@dbs.com">tszwangtam@dbs.com</a></td>
+					</tr>
+					<tr>
+						<td>Deutsche Bank / Peter Milliken</td>
+						<td><a href="mailto:peter.milliken@db.com">peter.milliken@db.com</a></td>
+					</tr>
+					<tr>
+						<td>HSBC / Neale Anderson</td>
+						<td><a href="mailto:neale.anderson@hsbc.com.hk">neale.anderson@hsbc.com.hk</a></td>
+					</tr>
+					<tr>
+						<td>Jefferies / Edison Lee</td>
+						<td><a href="mailto:edison.lee@jefferies.com">edison.lee@jefferies.com</a></td>
+					</tr>
+					<tr>
+						<td>JPMorgan / Michelle Wei</td>
+						<td><a href="mailto:michelle.z.wei@jpmorgan.com">michelle.z.wei@jpmorgan.com</a></td>
+					</tr>
+					<tr>
+						<td>Macquarie / Nicolas Baratte</td>
+						<td><a href="mailto:nicolas.baratte@macquarie.com">nicolas.baratte@macquarie.com</a></td>
+					</tr>
+					<tr>
+						<td>Mizuho Securities / Marvin Lo</td>
+						<td><a href="mailto:marvin.lo@hk.mizuho-sc.com">marvin.lo@hk.mizuho-sc.com</a></td>
+					</tr>
+					<tr>
+						<td>Morgan Stanley / Gary Yu</td>
+						<td><a href="mailto:gary.h.yu@morganstanley.com">gary.h.yu@morganstanley.com</a></td>
+					</tr>
+					<tr>
+						<td>Nomura / Bing Duan</td>
+						<td><a href="mailto:bing.duan1@nomura.com">bing.duan1@nomura.com</a></td>
+					</tr>
+					<tr>
+						<td>Sanford C. Bernstein / Chris Lane</td>
+						<td><a href="mailto:chris.lane@bernstein.com">chris.lane@bernstein.com</a></td>
+					</tr>
+					<tr>
+						<td>UBS / James Wang</td>
+						<td><a href="mailto:james-zb.wang@ubs.com">james-zb.wang@ubs.com</a></td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+		<p class="remark">Disclaimer: China Mobile Limited is followed by the analyst(s) listed above. This analyst list and their opinions may not be complete and is subject to change from time to time. Any information, statistics, opinion, forecasts or recommendation regarding the financial position and/or operating performance of China Mobile Limited made by these analysts are solely those of theirs and do not represent views or opinions of China Mobile Limited or its management, and reference above does not imply China Mobile Limited's endorsement of their independent views or opinions.</p>
+      </div>
+   </div>
+   
+<div class="sitemapWrapper">
+      <div class="row">
+      	<div class="breadCrumb">
+			<img src="../images/global/icon_logo.png" alt=""/ ><a href="../global/home.php">Home</a>
+            <img src="../images/global/breadcrumb_arrow.png" class="arrow"> Investor Relations
+<img src="../images/global/breadcrumb_arrow.png" class="arrow"> Analyst Coverage
+        </div>
+        <div class="sitemapContainer">
+        <div class="item">
+        	<div class="title">About China Mobile</div>
+            <ul class="sitemap">
+            	 <li><a href="../about/overview.php">Overview</a></li>
+                 <li><a href="../about/chairman.php">Chairman's Statement</a></li>
+                 <li><a href="../about/directors.php">Board of Directors and Senior Management</a></li>
+                 <li><a href="../about/milestones.php">Milestones</a></li>
+                 <li><a href="../about/awards.php">Recognition & Awards</a></li>
+                 <li><a href="../about/rating.php">Credit Ratings</a></li>
+                 <li><a href="../about/cg.php">Corporate Governance</a></li>
+                 <li><a href="../about/sd.php">Sustainable Development</a></li>
+            </ul>
+            <div class="clear"></div>
+        </div>
+        <div class="item">
+        	<div class="title">Investor Relations</div>
+            <ul class="sitemap">
+            	 <li><a href="../ir/operation_m.php">Monthly Customer Data</a></li>
+            	 <li><a href="../ir/operation_q.php">Key Operation Data</a></li>
+            	 <li><a href="../ir/financial.php">Financial Review</a></li>
+                 <li><a href="../ir/reports.php">Financial Reports</a></li>
+                 <li><a href="../ir/webcasts.php">Presentations / Webcasts</a></li>
+                 <li><a href="../ir/announcements.php">Announcements</a></li>
+                 <li><a href="../ir/sec.php">US SEC Filings</a></li>
+                 <li><a href="../ir/circulars.php">Circulars & Other Documents</a></li>
+                 <li><a href="../ir/corpactions.php">Corporate Actions</a></li>
+                 <li><a href="../ir/stockquote.php">Shareholders Service</a></li>
+                 <li><a href="../ir/ircalendar.php">IR Calendar</a></li>
+                 <li><a href="../ir/analyst.php">Analyst Coverage</a></li>
+                 <li><a href="../ir/faq.php">FAQs</a></li>
+                 <li><a href="../ir/ircontact.php">IR Contact</a></li>
+            </ul>
+        </div>
+        <div class="item">
+        	<div class="title">Media Centre</div>
+            <ul class="sitemap">
+            	 <li><a href="../media/press.php">Press Releases</a></li>
+                 <li><a href="../media/news.php">News Highlights</a></li>
+                 <li><a href="../media/gallery.php">Photo Gallery</a></li>
+                 <li><a href="../media/video.php">Video Gallery</a></li>
+                 <li><a href="../media/corpimage.php">Corporate Images</a></li>
+                 <li><a href="../media/contact.php">Media Contact</a></li>
+            </ul>
+        </div>
+        <div class="item">
+        	<div class="title">Our Business</div>
+            <ul class="sitemap">
+            	 <li><a href="../business/business.php">Business Review</a></li>
+                 <li><a href="../business/service.php">Products & Services</a></li>
+                 <li><a href="../business/networks.php">Networks & Technologies</a></li>
+            </ul>
+        </div>
+		  </div>
+        <div class="item tools">
+        	<div class="title">Tools</div>
+            <ul class="sitemap">
+            	                  <li><a href="../global/rss.php">RSS Feeds</a></li>
+                 <li><a href="#" class="print">Print this Page</a></li>
+                 <li><a href="#" class="bookmark">Add to Favourites</a></li>
+                 <li><a href="../webform/emailpage.php?page=https://www.chinamobileltd.com/en/ir/analyst.php" id="emailpage" >Email this Page</a></li>               
+                 <li><a href="#" class="addthis_button">Share this Page</a></li>
+                 <li><a href="../download/index.php">Download Centre</a></li>
+            </ul>
+        </div>
+        <div class="clear"></div>
+   </div>
+   </div>
+   <div class="footerWrapper">
+      <a href="#" class="backToTop"></a>
+      <div class="row">
+		  <div class="copyright">&copy; Copyright 2020 China Mobile Limited. <span>All rights reserved. <a href="../global/termsofuse.php"><span class="bold">Website Terms of Use</span></a></span></div>
+         <div class="footerLang">Language: <a href="javascript:changeLanguage('en');" class="langBtn_en">English</a>  |  <a href="javascript:changeLanguage('tc');" class="langBtn_tc">繁體</a>  |  <a href="javascript:changeLanguage('sc');" class="langBtn_sc">简体</a></div>
+      </div>
+   </div></div>
+<script type="text/javascript" src="../../js/jquery-1.11.1.min.js"></script>
+<script type="text/javascript" src="../../js/fancybox/jquery.fancybox.js?v=2.1.5.js"></script>
+<script type="text/javascript" src="../../js/jquery.cycle2.min.js"></script>
+<script type="text/javascript" src="../../js/highcharts.js"></script>
+<script type="text/javascript" src="../../js/exporting.js"></script>
+<script type="text/javascript" src="../../js/jquery.appear.js"></script>
+<script type="text/javascript" src="../../js/en/chart_common.js"></script>
+
+<script type="text/javascript" src="../../js/slick/slick.min.js"></script>
+<script type="text/javascript" src="../../js/magnific.js"></script>
+<script type="text/javascript" src="../../js/jquery.responsiveTabs.min.js"></script>
+<script type="text/javascript" src="../../js/common.js"></script>
+<script language="javascript" type="text/javascript" src="//s7.addthis.com/js/250/addthis_widget.js"></script>
+
+
+
+
+<script>
+
+</script>
+</body>
+</html>
